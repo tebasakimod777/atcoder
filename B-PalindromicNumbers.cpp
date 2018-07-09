111db12eff4794305a178762b74133d7f995bca1
tebasakioisii <tebasaki.kawasaki@gmail.com>
2018-07-09 17:41:51 +0900

    A-DiagonalString.cpp

---
+++ B-PalindromicNumbers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S1, S2, S3;
+
+    cin >> S1 >> S2 >> S3;
+
+    cout << S1.at(0) << S2.at(1) << S3.at(2) << endl;
+    
+    return 0;
+}
