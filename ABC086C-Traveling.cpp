df529c9a51f023ebdf9184c7e3e8a2fcd8dc4035
tebasakioisii <tebasaki.kawasaki@gmail.com>
2018-07-09 17:29:51 +0900

    Complete ABCSelection

--- ABC086C-Traveling.cpp
+++ ABC086C-Traveling.cpp
@@ -7,36 +7,32 @@ int main() {
 
     cin >> N;
 
-    cout << N << endl;
     N += 1;
-    cout << N << endl;
+
     vector<int> t(N), x(N), y(N);
 
-    cout << N << endl;
-    t.at(0) = x.at(0) = y.at(0) = 0;
+    N -= 1;
 
-    cout << N << endl;
+    t[0] = x[0] = y[0] = 0;
+
     for (i = 0; i < N; i++) cin >> t.at(i + 1) >> x.at(i + 1) >> y.at(i + 1);
-    cout << N << endl;
 
     for( i = 0; i < N; i++) {
-    cout << N << endl;
         v = abs(x.at(i + 1) - x.at(i)) + abs(y.at(i + 1) - y.at(i));
-    cout << N << endl;
         t_amount = t.at(i + 1) - t.at(i);
-    cout << N << endl;
-        if ( !(t_amount <= v)) {
+        if ( t_amount < v) {
             can = false;
             break;
         }
+
         if ( !(t_amount % 2 == v % 2)) {
             can = false;
             break;
         }
     }
 
-    if (can) cout << "YES" << endl;
-    else cout << "NO" << endl;
+    if (can) cout << "Yes" << endl;
+    else cout << "No" << endl;
 
     return 0;
 }
