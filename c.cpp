e3d89847a912384ebd1d41fe37c70d44defa7994
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-09-23 23:23:05 +0900

    c end

--- c.cpp
+++ c.cpp
@@ -3,4 +3,38 @@ using namespace std;
 
 int main() {
 
+    string S, T;
+    vector<int> s, t;
+    char a, b;
+
+    cin >> S;
+    cin >> T;
+    
+    sort(S.begin(), S.end());
+    sort(T.begin(), T.end());
+
+    for (int i = 0; i < S.size(); i++) {
+        
+        if (S.at(i) == a){
+            s.end()++;
+        } else {
+            s.push_back(1);
+            a = S.at(i);
+        }
+
+        if (S.at(i) == b) {
+            t.end()++;
+        } else {
+            t.push_back(1);
+            b = S.at(i);
+        }
+    }
+
+    if (s == t) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+
+    return 0;
 }
