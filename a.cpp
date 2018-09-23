636744ce7a66fdbfd0b3fdbf0fe7d33fabdfb4fc
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-09-23 21:20:33 +0900

    A Maximize the Formula

--- a.cpp
+++ a.cpp
@@ -1,6 +1,17 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    int inputs[3];
+    int tmp, count, x, y;
+    scanf("%d %d %d", &inputs[0], &inputs[1], &inputs[2]);
 
+    sort(inputs, inputs + 3);
+
+    x = inputs[2] * 10 + inputs[1];
+    y = inputs[0];
+
+    printf("%d\n", x + y);
+
+    return 0;
 }
