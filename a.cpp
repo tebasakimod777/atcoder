efe4e5044904284ce02b4fa4e494be4221831e0c
tebasakioisii <tebasaki@tebasakiMBP.local>
2018-10-06 20:23:21 +0900

    setup for ABC112

--- a.cpp
+++ a.cpp
@@ -2,16 +2,6 @@
 using namespace std;
 
 int main() {
-    int inputs[3];
-    int tmp, count, x, y;
-    scanf("%d %d %d", &inputs[0], &inputs[1], &inputs[2]);
-
-    sort(inputs, inputs + 3);
-
-    x = inputs[2] * 10 + inputs[1];
-    y = inputs[0];
-
-    printf("%d\n", x + y);
 
     return 0;
 }
