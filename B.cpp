8d2057ae318e4a20cdf86bbe3779be50c9e30771
tebasaki <tebasaki.wejobs@gmail.com>
2018-12-18 18:47:56 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
+    const string boin = "aiueo";
+    string remark, nuki;
+    bool flg;
 
+    int i, j;
+    cin >> remark;
+
+    nuki.resize(remark.size());
+    for (i = 0; i < remark.size(); i++) {
+        flg = true;
+        for (j = 0; j < boin.size(); j++) {
+            if (remark.at(i) == boin.at(j)) {
+                flg = false;
+            }
+        }
+        if (flg) {
+            cout << remark.at(i);
+        }
+    }
+    putchar('\n');
     return 0;
 }
