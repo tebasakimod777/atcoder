1a88189523599bc87be78de681373af17fd3a14b
tebasaki <tebasaki.wejobs@gmail.com>
2018-12-21 16:44:40 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
 int main() {
+    vector<int> sum(4);
+    int size, temp;
+    string a;
+    
+    cin >> size;
+    cin >> a;
+
+    for (int i = 1; i <= 4; i++) {
+
+        for (int j = 0; j < size; j++) {
+            if(a.at(j) == '0' + i) {
+                sum[i - 1]++;
+            }            
+        }    
+    }
+
+    sort(sum.begin(), sum.end());
+
+    printf("%d %d\n", sum.at(3), sum.at(0));
 
     return 0;
 }
