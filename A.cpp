c5322d648f71f40cc33f3153e1082ab06fb46eae
tebasaki <tebasaki.wejobs@gmail.com>
2018-12-22 21:27:37 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
+    string input;
+    int ans = 0;
 
+    cin >> input;
+
+    for (int i = 0; i < input.size(); i++) {
+        if (input.at(i) == '0' + 2 ) {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
     return 0;
 }
