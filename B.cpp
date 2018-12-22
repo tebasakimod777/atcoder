69d9ecaa2de393c66fe7d5a9280d71e8b36ff243
tebasaki <tebasaki.wejobs@gmail.com>
2018-12-22 21:32:31 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
+    int n, h, w, ans = 0;
+    vector<int> a, b;
+
+    cin >> n >> h >> w;
+
+    a.resize(n);
+    b.resize(n);
+
+    for (int i = 0; i < n; i++) 
+        cin >> a.at(i) >> b.at(i);
+    
+    for (int i = 0; i < n; i++) {
+        if (a.at(i) >= h && b.at(i) >= w)
+            ans++;
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
