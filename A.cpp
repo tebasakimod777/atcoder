6f664089f98358b56bde10e108ec3d1e83debae8
tebasaki <tebasaki.wejobs@gmail.com>
2019-01-14 00:30:05 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FOR_i(n) for (int i = 0; i <= n; i++)
+
 int main() {
+    double N, sum;
+
+    cin >> N;
+
+    FOR_i(N) {
+        sum += (double)i;
+    }
+
+    cout << sum / N * 10000 << endl;
 
     return 0;
 }
