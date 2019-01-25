39ec885633407fd4c1be9598124743c74f96ca56
tebasaki <tebasaki.wejobs@gmail.com>
2019-01-25 21:19:31 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main() {
+    double n, num = 0;
+    string r;
+
+    cin >> n >> r;
+
+    for(int i = 0;i < n; i++) {
+        if (r.at(i) == 'A')
+            num += 4;
+        if (r.at(i) == 'B')
+            num += 3;
+        if (r.at(i) == 'C')
+            num += 2;
+        if (r.at(i) == 'D')
+            num += 1;
+    }
+
+    printf("%.14lf", num / n);
 
     return 0;
 }
