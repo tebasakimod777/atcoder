3b50230723189ccfda1051b07a340069cd74b72a
tebasaki <tebasaki.wejobs@gmail.com>
2019-01-27 21:08:29 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
+    int n, a, b, a1, a2;
+
+    cin >> n >> a >> b;
+
+    a1 = abs(a - b);
+    a2 = abs(max(a, b) - a1);
+    
+    a1 = a + b - n;
+
+    if (a1 < 0) a1 = 0;
+
+    printf("%d %d\n", a2, a1);
 
     return 0;
 }
