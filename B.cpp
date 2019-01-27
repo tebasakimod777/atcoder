b2700b0cbcfe75c22771f54c0d9b5864a88d3c4f
tebasaki <tebasaki.wejobs@gmail.com>
2019-01-27 21:26:19 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -3,5 +3,23 @@ using namespace std;
 
 int main() {
 
+    int n, miss, ans;
+    string a, b, c;
+    
+    cin >> n >> a >> b >> c;
+    
+    ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        miss = 0;
+        if(a.at(i) != b.at(i)) miss++;
+        if(b.at(i) != c.at(i)) miss++;
+        if(c.at(i) != a.at(i)) miss++;
+
+        if(miss > 0) ans += miss - 1;
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
