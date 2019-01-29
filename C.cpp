8c0a04b86dfdf4dd6e238c86eab6a3fe861aca4a
tebasaki <tebasaki.wejobs@gmail.com>
2019-01-29 23:23:10 +0900

    C.cpp

--- C.cpp
+++ C.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main() {
+    int n, a, b;
+    long long maxt = 0, maxa = 0;
+
+    cin >> n;
+
+    vector<int> sum(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a >> b;
+        sum.at(i) = a + b;
+        maxt += b;
+    }
+
+    sort(sum.begin(), sum.end(), greater<int>());
+
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0)
+        maxa += sum.at(i);
+    }
+
+    cout << maxa - maxt << endl;
 
     return 0;
 }
