bb74fbe3debee07678a85143d53da0b95c378c41
tebasaki <tebasaki.wejobs@gmail.com>
2019-03-03 21:07:01 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main() {
 
+    int a, b, c, i;
+
+    cin >> a >> b >> c;
+
+    for(i = 0; i < c; i++) {
+        if(b < a * (i + 1)) {
+            break;
+        }
+    }
+
+    cout << i << endl;
+
     return 0;
 }
