17252c3234ceeefb29b3f7d96370227405b03c20
tebasaki <tebasaki.wejobs@gmail.com>
2019-03-03 21:42:01 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -3,5 +3,20 @@ using namespace std;
 
 int main() {
 
+    int a, b, k, i, count;
+
+    cin >> a >> b >> k;
+
+    count = 0;
+    for(i = a; i > 0; i--) {
+        if((a % i == 0) && (b % i == 0)) {
+            // cout << i << endl;
+            count++;
+        }
+        if(k == count) break;
+    }
+    
+    cout << i << endl;
+
     return 0;
 }
