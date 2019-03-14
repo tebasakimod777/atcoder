8212599e763244516e67a6f78fba96b605607cbf
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-14 15:22:06 +0900

    C.cpp

--- C.cpp
+++ C.cpp
@@ -1,53 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve(string s);
-
 int main() {
-    string s;
-    int count, a, b;
-    a = b = 0;
-
-    cin >> s;
+    string s; cin >> s;
+    int blue = 0;
 
     for(int i = 0; i < s.size(); i++) {
-        if(s.at(i) == '0') a++;
-        else b++;
+        if (s.at(i) - '0' == 1) {
+            blue++;
+        }
     }
-    
-    if(a > b) count = 2 * b;
-    else count = 2 * a;
 
-    // count = solve(s);
-    
-    cout << count << endl;
+    if(blue < s.size() - blue) cout << blue * 2 << endl;
+    else cout << (s.size() - blue) * 2 << endl;
 
     return 0;
 }
-
-// int solve(string s) {
-//     int count, flg;
-
-//     do {
-//         flg = 1;
-//         if(s.size() < 2) break;
-//         for(int i = 0; i < (s.size() - 1); i++) {
-//             while((s.at(i) == '0' && s.at(i + 1) == '1') || (s.at(i) == '1' && s.at(i + 1) == '0')) {
-//                 // cout << i << endl;a
-//                 count += 2;
-//                 s.erase(s.begin() + i);
-//                 s.erase(s.begin() + i);
-//                 // cout << i << endl;
-
-//                 flg = 0;
-//                 if(s.size() == 0) return count;
-//                 if(i >= (s.size() - 1))
-//                     break;
-//             }
-//             // cout << "break" << endl;
-//         }
-//         // cout << count << endl;
-//     } while(flg == 0);
-
-//     return count;
-// }
