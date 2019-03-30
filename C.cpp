b93bad75c8361f6c6783b85c59cab2cfa4dab2f0
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-30 21:35:56 +0900

    C.cpp

--- C.cpp
+++ C.cpp
@@ -1,9 +1,22 @@
 #include <bits/stdc++.h>
+#define f(i, n) for(int i = 0; i < n; i++)
 using namespace std;
 
 typedef long long ll;
 
 int main() {
+    ll n, q, sur = 0;
+    char s, t;
+    string s;
+
+    cin >> n >> q >> s;
+
+    f(i, q) {
+        cin >> s >> t;
+
+    }
+
+    puts(sur);
 
     return 0;
 }
