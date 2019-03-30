ddf54eeaf18aa3d5d01ed469e4ffaa7da57ff2bd
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-30 21:04:18 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -4,6 +4,13 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if( a == b && b == c)
+        puts("Yes");
+    else puts("No");
 
     return 0;
 }
