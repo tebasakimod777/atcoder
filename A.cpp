a79aea218412450cc0256e0cca34a9f7842043cd
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-30 19:03:37 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 typedef long long ll;
 
+int solve(string s);
+
 int main() {
+    string inp;
+    int flag = 0;
+    
+    cin >> inp;
+
+
+    while(flag == 0) {
+        flag = 1;
+    
+        for(int i = 0; i < inp.size() - 1; i++) {
+            if( (inp.at(i) == 'W') && (inp.at(i+1) == 'A')) {
+                inp.at(i) = 'A';
+                i++;
+                inp.at(i) = 'C';
+                flag = 0;
+            }
+        }
+    }
+    
+    cout << inp << endl;
 
     return 0;
 }
