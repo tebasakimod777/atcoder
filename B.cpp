ae8a740614a8d8c3366225739b5ae7614a6b000b
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-30 21:14:51 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -4,6 +4,19 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    int n, sum = 0;
+    string s;
+    cin >> n >> s;
+
+    for(int i = 0; i < n; i++) {
+        if(s.at(i) - 'R' == 0) {
+            sum++;
+        }
+    }
+
+    cout << s << endl;
+    if(sum > (n / 2)) puts("Yes");
+    else puts("No");
 
     return 0;
 }
