4996e68ce9339f0a1f9f202b2aaf2d560892edb7
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-03-30 21:41:57 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -4,6 +4,16 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    char a, b;
+
+    cin >> b;
+
+    if(b == 'A') a = 'T';
+    else if(b == 'C') a = 'G';
+    else if(b == 'G') a = 'C';
+    else if(b == 'T') a = 'A';
+
+    cout << a << endl;
 
     return 0;
 }
