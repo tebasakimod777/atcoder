d28ae1ae855fb9f238d4a93aa22af08482eb6d0d
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-04-02 10:15:34 +0900

    B.cpp

--- B.cpp
+++ B.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
+#define f(i, n) for(int i = 0; i < n; i++)
 using namespace std;
 
 typedef long long ll;
 
 int main() {
+    string s;
+    int sum, mx;
+
+    cin >> s;
+
+    sum = mx = 0;
+
+    f(i, s.size()) {
+        if(s.at(i) =='A' || s.at(i) =='C' || s.at(i) =='G' || s.at(i) =='T') {
+            sum++;
+        } else {
+            mx = max(mx, sum);
+            sum = 0;
+        }
+    }
+
+    mx = max(mx, sum);
+
+    cout << mx << endl;
 
     return 0;
 }
