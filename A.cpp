60a7a21b3e463b2d57e7fbf7d3945e3aedff4a1b
tebasakioisii <tebasaki.wejobs@gmail.com>
2019-04-07 11:19:34 +0900

    A.cpp

--- A.cpp
+++ A.cpp
@@ -6,6 +6,23 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    vector<int>  spot(5);
+    int k;
+ 
+    f(i, 5) cin >> spot.at(i);
+    cin >> k;
+ 
+    int flg = 1;
+    f(i, 3) {
+        for(int j = i + 1; j < 5; j++) {
+            // cout << i << endl;
+            if(abs(spot.at(i) - spot.at(j)) > k)
+                flg = 0;
+        }
+    }
+ 
+    if(flg == 0) puts(":(");
+    else puts("Yay!");
 
     return 0;
 }
