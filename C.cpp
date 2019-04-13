fe10eb4f89a4eac96f108b0011a304f431b3ea6a
tebasaki <tebasaki.wejobs@gmail.com>
2019-04-13 18:46:40 +0900

    C.cpp

--- C.cpp
+++ C.cpp
@@ -6,6 +6,22 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    ll A, B, C, X, Y, price, minX, minY;
+
+    cin >> A >> B >> C >> X >> Y;
+
+    price = (A * X) + (B  * Y);
+
+    minX = X * 2 * C;
+    minY = Y * 2 * C;
+
+    if ( X > Y) {
+        int mod = (X - Y) * A;
+        cout << min (minX, min(minY + mod, price)) << endl;
+    } else {
+        int mod = (Y - X) * B;
+        cout << min(minY, min(mod + minX, price)) << endl;
+    }
 
     return 0;
 }
