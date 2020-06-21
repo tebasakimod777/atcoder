37665a05608263a012852fd43c9639d57668c613
tebasakioisii <tebasaki.wejobs@gmail.com>
2020-06-21 20:19:08 +0900

    setup for ABC

--- A.cpp
+++ A.cpp
@@ -1,12 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
 
 #define f(i, q) for(int i = 0; i < q; i++)  
 
 typedef long long ll;
 
 int main() {
+    ll i, j, k;
+    ll ans = 0;
 
+
+
+    printf("lld\n", ans);
 
     return 0;
 }
--- F.cpp
+++ F.cpp
@@ -1,15 +1,11 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 #define f(i, q) for(int i = 0; i < q; i++)  
 
 typedef long long ll;
 
 int main() {
-    ll i, j, k;
-    ll ans = 0;
-
-
-    printf("%lld\n", ans);
 
     return 0;
 }
