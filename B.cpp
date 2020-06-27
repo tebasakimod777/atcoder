07f46f7b39a2a7c21ae1ff7d80f5b66dbae9a524
tebashi <tebasaki.wejobs@gmail.com>
2020-06-27 20:36:09 +0900

    modify files

--- B.cpp
+++ B.cpp
@@ -1,11 +1,27 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 #define f(i, q) for(int i = 0; i < q; i++)  
 
 typedef long long ll;
 
+// 昇順にソート
+int asc_comp(const void* a, const void* b){
+    ll x = *(ll*)a;
+    ll y = *(ll*)b;
+    return x - y;
+}
+// 降順にソート
+int dec_comp(const void* a, const void* b){
+    ll x = *(ll*)a;
+    ll y = *(ll*)b;
+    return y - x;
+}
+
 int main() {
+    // qsort(in, a, sizeof(ll), dec_comp);
 
     return 0;
 }
