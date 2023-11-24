c09b184379a3daf46030064adfbcf879607cad71
tebii <tebasaki.wejobs@gmail.com>
2023-11-24 21:43:22 +0900

    mainブランチのみで作業するように変更

---
+++ ABC/300s/329/E/E.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(a)  (a).begin(),(a).end()
+typedef long long ll;
+using pint = pair<int, int>;
+using pstring = pair<int, string>;
+
+int main() {
+    ll ans;
+    ll N;
+    cin >> N;
+
+    cout << ans << endl;
+
+    return 0;
+}
